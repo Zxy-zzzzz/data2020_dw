84e57ec4e9ad3d2257a3aa8bfd0682a9932cf2bd
zxytyty <1145745326@qq.com>
2020-12-07 16:37:20 +0800

    第一次提交

---
+++ dw-logger/src/main/java/com/bigdata/dwlogger/DwLoggerApplication.java
@@ -0,0 +1,15 @@
+package com.bigdata.dwlogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class DwLoggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DwLoggerApplication.class, args);
+    }
+
+}
